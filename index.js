2e20b0f0ee3b149eb9956064c1bc3af7ae050d95
santosh <santosh.ece06@gmail.com>
2018-12-13 16:10:19 +0530

    update.

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-
 import {Second as DefaultRoot} from './src';
 
 AppRegistry.registerComponent(appName, () => DefaultRoot);
